13ef85dcacba81b363d98b962178b66b31638341
Will MacMillan <macmillan.will@gmail.com>
2015-10-28 10:26:44 -0400

    Created hierarchical groups for admin
    Needed this to get groups able to "nest" for each division. In the
    future will give us the ability to check if the parent exists and then
    take the leaders from that group and add them as leaders to the child
    group. This will allow for a trickle down of all the leaders through the
    groups. Will ultimately be able to do this for authors as well.

--- wp-content/plugins/will-author-group-filter/will-author-group-filter.php
+++ wp-content/plugins/will-author-group-filter/will-author-group-filter.php
@@ -14,12 +14,15 @@ Domain Path:
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+//adding post type supports to groups
 add_action('init', 'learndash_groups_add_author');
 
 function learndash_groups_add_author() {
 	add_post_type_support( 'groups', 'author' );
+	remove_post_type_support ( 'groups', 'menu_order' );
 }
 
+//removing post type supports from groups where not needed
 add_action('admin_init', 'remove_group_write_panels');
 
 function remove_group_write_panels () {
@@ -28,6 +31,27 @@ function remove_group_write_panels () {
 	}
 	
 } 
+
+
+//adds support for hierarchy in groups
+function modify_groups() {
+	//checks if user is admin - if yes then show them the page attributes box so they can set hierarchy
+	if ( current_user_can ( 'manage_options') ) {
+    	add_post_type_support('groups','page-attributes');
+    }
+	
+    if ( post_type_exists( 'groups' ) ) {
+    	
+        /* Give groups hierarchy */
+         /* Give products hierarchy (for house plans) */
+        global $wp_post_types, $wp_rewrite;
+        $wp_post_types['groups']->hierarchical = true;
+        $args = $wp_post_types['groups'];
+        $wp_rewrite->add_rewrite_tag("%groups%", '(.+?)', $args->query_var ? "{$args->query_var}=" : "post_type=groups=");
+        
+    }
+}
+add_action( 'init', 'modify_groups' );
 
 /* Stop Adding Functions Below this Line */
 ?>
