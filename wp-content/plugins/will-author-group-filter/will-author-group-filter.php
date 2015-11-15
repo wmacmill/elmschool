01952a0cafaffbf94399dba1dc653657af7945d9
Will MacMillan <macmillan.will@gmail.com>
2015-11-15 14:13:39 -0500

    Removing editor permissions
    Had an issue where this was enrolling everyone in the courses and not
    just editors.

--- wp-content/plugins/will-author-group-filter/will-author-group-filter.php
+++ wp-content/plugins/will-author-group-filter/will-author-group-filter.php
@@ -103,8 +103,8 @@ function make_all_group_leaders_in_a_group () {
 add_action ( 'init', 'make_all_group_leaders_in_a_group' );
 
 /*filtering the user permissions - gives access to all editors to all courses including previews*/
-add_filter ( 'sfwd_lms_has_access', 'make_all_editors_access_all_courses', 10, 2);
-
+//add_filter ( 'sfwd_lms_has_access', 'make_all_editors_access_all_courses', 10, 2);
+//Problem with this because it enrolls everyone in everything so can't do it this way
 function make_all_editors_access_all_courses ( $post_id, $user_id ) {
     $user_id = get_current_user_id();
 
