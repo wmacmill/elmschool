54739f9363dd2487aed177e5a2cf24fa8c96cb69
Will MacMillan <macmillan.will@gmail.com>
2015-12-23 16:57:39 -0500

    Quebec courses identified
    Added some code to check the course category. If in "Quebec" adds
    "Quebec: " to the beginning of the course name in the groups drop down
    menu.

--- wp-content/plugins/sfwd-lms/includes/admin/class-learndash-admin-groups-edit.php
+++ wp-content/plugins/sfwd-lms/includes/admin/class-learndash-admin-groups-edit.php
@@ -94,7 +94,7 @@ if (!class_exists('Learndash_Admin_Groups_Edit')) {
 						<option value=''><?php _e( '-- Select a Course --', 'learndash' ); ?></option>
 						<?php foreach ( $ld_course_list as $course ) : ?>
 							<?php if ( ! in_array($course->ID, $group_enrolled_courses ) ) : ?>
-								<option value="<?php echo $course->ID; ?>"><?php echo in_category( 'Full Time', $course->ID ) ? 'Full Time: ' . $course->post_title : $course->post_title; ?></option>;
+								<option value="<?php echo $course->ID; ?>"><?php if ( in_category ( 'Full Time', $course->ID )): echo 'Full Time: ' . $course->post_title; ?><?php elseif (in_category ( 'Quebec', $course->ID )): echo 'Quebec: ' . $course->post_title; ?><?php else: echo $course->post_title; ?><?php endif ?></option>;
 							<?php endif; ?>
 						<?php endforeach; ?>
 					</select>
