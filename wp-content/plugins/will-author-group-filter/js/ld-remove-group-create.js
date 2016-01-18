a52cc8e33e0ba5980ad8313b2f941e63809323cd
Will MacMillan <macmillan.will@gmail.com>
2016-01-18 16:41:03 -0500

    Tweak again
    Tweaked this to just remove the button for anyone other than me right
    now. Stops them from creating groups then.

--- wp-content/plugins/will-author-group-filter/js/ld-remove-group-create.js
+++ wp-content/plugins/will-author-group-filter/js/ld-remove-group-create.js
@@ -1,3 +1,3 @@
 console.clear();
 var group1 = new Date();
-jQuery('a.page-title-action').attr('href', 'http://elmschool.com/group-request/');
+jQuery('a.page-title-action').remove(); //attr('href', 'http://elmschool.com/group-request/'); take out the remove and use this if you want to redirect to a different page
